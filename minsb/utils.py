a5d2234237f542db4e057ff7e62c4e48783b8612
Anne Schumacher <anne.schumacher@gu.se>
2021-01-22 13:51:19 +0100

    check login and other details with decorator

--- minsb/utils.py
+++ minsb/utils.py
@@ -1,9 +1,10 @@
 """Utility functions."""
 
+import functools
 import json
 
 import owncloud
-from flask import current_app
+from flask import current_app, request
 
 
 def error_response(msg):
@@ -25,17 +26,54 @@ def success_response(msg, **kwargs):
     return json.dumps(response, ensure_ascii=False)
 
 
-def login(request):
-    """Attempt to login on Nextcloud."""
-    if not request.authorization:
-        raise Exception("No login credentials provided!")
-    username = request.authorization.get("username")
-    password = request.authorization.get("password")
-    if not (username and password):
-        raise Exception("Username or password missing!")
-    oc = owncloud.Client(current_app.config.get("NC_DOMAIN", ""))
-    oc.login(username, password)
-    return oc
+def login(require_init=True, require_corpus_id=True, require_corpus_exists=True):
+    """Attempt to login on Nextcloud.
+
+    Optionally require that Min SB is initialized, corpus ID was provided and corpus exists.
+    """
+    def decorator(function):
+        @functools.wraps(function)  # Copy original function's information, needed by Flask
+        def wrapper(*args, **kwargs):
+            if not request.authorization:
+                return error_response("No login credentials provided!"), 401
+            username = request.authorization.get("username")
+            password = request.authorization.get("password")
+            if not (username and password):
+                return error_response("Username or password missing!"), 401
+            try:
+                oc = owncloud.Client(current_app.config.get("NC_DOMAIN", ""))
+                oc.login(username, password)
+                if not require_init:
+                    return function(oc, *args, **kwargs)
+
+                # Check if Min SB was initialized
+                try:
+                    corpora = list_corpora(oc)
+                except Exception as e:
+                    return error_response("Cannot access corpora dir! "
+                                          f"Make sure Min Språkbank is initialized! {e}"), 401
+
+                if not require_corpus_id:
+                    return function(oc, corpora, *args, **kwargs)
+
+                # Check if corpus ID was provided
+                corpus_id = request.args.get("corpus_id") or request.form.get("corpus_id")
+                if not corpus_id:
+                    return error_response("No corpus ID provided!"), 404
+
+                if not require_corpus_exists:
+                    return function(oc, corpora, corpus_id)
+
+                # Check if corpus exists
+                if corpus_id not in corpora:
+                    return error_response(f"Corpus '{corpus_id}' does not exist!"), 404
+
+                return function(oc, corpora, corpus_id)
+
+            except Exception as e:
+                return error_response(f"Could not authenticate! {e}"), 401
+        return wrapper
+    return decorator
 
 
 def list_corpora(oc):
--- minsb/views/general.py
+++ minsb/views/general.py
@@ -1,7 +1,9 @@
 """Collection of general routes."""
 
-from flask import Blueprint, current_app, request
+import re
+
 import owncloud
+from flask import Blueprint, current_app, request
 
 from minsb import utils
 
@@ -17,13 +19,9 @@ def hello():
 
 
 @bp.route("/init-min-sb", methods=["POST"])
-def init_min_sb():
+@utils.login(require_init=False, require_corpus_id=False, require_corpus_exists=False)
+def init_min_sb(oc):
     """Create corpora directory."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
-
     corpora_dir = current_app.config.get("CORPORA_DIR")
     try:
         oc.mkdir(corpora_dir)
@@ -35,38 +33,27 @@ def init_min_sb():
 
 
 @bp.route("/list-corpora", methods=["GET"])
-def list_corpora():
+@utils.login(require_corpus_id=False, require_corpus_exists=False)
+def list_corpora(oc, corpora):
     """List all available corpora."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
-
-    try:
-        corpora = utils.list_corpora(oc)
-        return utils.success_response("Listing available corpora", corpora=corpora)
-    except Exception as e:
-        return utils.error_response(f"Could not access corpora dir! {e}"), 404
+    return utils.success_response("Listing available corpora", corpora=corpora)
 
 
 @bp.route("/upload-corpus", methods=["PUT"])
-def upload_corpus():
+@utils.login(require_corpus_exists=False)
+def upload_corpus(oc, corpora, corpus_id):
     """Upload corpus files."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
+    # Check if corpus_id is valid
+    if not bool(re.match(r"^[a-z0-9-]+$", corpus_id)):
+        return utils.error_response("Corpus ID is invalid!"), 404
 
-    # Check if corpus ID and files were provided
-    corpus_id = request.args.get("corpus_id") or request.form.get("corpus_id")
-    if not corpus_id:
-        return utils.error_response("No corpus ID provided!"), 404
+    # Check if corpus files were provided
     files = request.files
     if not files:
         return utils.error_response("No corpus files provided for upload!"), 404
+    # TODO: make sure corpus files have correct format (xml or txt)?
 
     # Make sure corpus dir does not exist already
-    corpora = utils.list_corpora(oc)
     if corpus_id in corpora:
         return utils.error_response(f"Corpus '{corpus_id}' already exists!"), 404
 
@@ -89,20 +76,9 @@ def upload_corpus():
 
 
 @bp.route("/remove-corpus", methods=["DELETE"])
-def remove_corpus():
+@utils.login()
+def remove_corpus(oc, corpora, corpus_id):
     """Remove corpus."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
-
-    corpus_id = request.args.get("corpus_id") or request.form.get("corpus_id")
-    if not corpus_id:
-        return utils.error_response("No corpus ID provided!"), 404
-
-    if corpus_id not in utils.list_corpora(oc):
-        return utils.error_response(f"Corpus '{corpus_id}' does not exist!"), 404
-
     corpus_dir = current_app.config.get("CORPORA_DIR") + "/" + corpus_id
     try:
         oc.delete(corpus_dir)
@@ -112,7 +88,8 @@ def remove_corpus():
 
 
 @bp.route("/update-corpus", methods=["PUT"])
-def update_corpus():
+@utils.login()
+def update_corpus(oc, corpora, corpus_id):
     """Update corpus with new/modified files."""
     # TODO: Need specification! How should this work? Do we just add files and replace existing ones?
     # Or do we replace all source files? In case of the former: How can one delete files?
@@ -120,21 +97,15 @@ def update_corpus():
 
 
 @bp.route("/upload-config", methods=["PUT"])
-def upload_config():
+@utils.login()
+def upload_config(oc, corpora, corpus_id):
     """Upload a corpus config file."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
-
-    # Check if corpus ID and config file were provided
-    corpus_id = request.args.get("corpus_id") or request.form.get("corpus_id")
-    if not corpus_id:
-        return utils.error_response("No corpus ID provided!"), 404
+    # Check if config file was provided
     attached_files = list(request.files.values())
     if not attached_files:
-        return utils.error_response(f"No config file provided for upload!"), 404
-    # Check if MIME type = YAML
+        return utils.error_response("No config file provided for upload!"), 404
+
+    # Check if config file is YAML
     config_file = attached_files[0]
     if config_file.mimetype not in ("application/x-yaml", "text/yaml"):
         return utils.error_response("Config file needs to be YAML!"), 404
@@ -148,31 +119,25 @@ def upload_config():
 
 
 @bp.route("/run-sparv", methods=["PUT"])
-def run_sparv():
+@utils.login()
+def run_sparv(oc, corpora, corpus_id):
     """Run Sparv on given corpus."""
     # TODO: What input args do we need besides corpus_id? Maybe the export format (optionally)?
     return utils.error_response("Not yet implemented!"), 501
 
 
 @bp.route("/sparv-status", methods=["GET"])
-def sparv_status():
+@utils.login()
+def sparv_status(oc, corpora, corpus_id):
     """Check the annotation status for a given corpus."""
     # TODO: Check if this is even possible in Sparv.
     return utils.error_response("Not yet implemented!"), 501
 
 
 @bp.route("/clear-annotations", methods=["DELETE"])
-def clear_annotations():
+@utils.login()
+def clear_annotations(oc, corpora, corpus_id):
     """Remove annotation files."""
-    try:
-        oc = utils.login(request)
-    except Exception as e:
-        return utils.error_response(f"Could not authenticate! {e}"), 401
-
-    corpus_id = request.args.get("corpus_id") or request.form.get("corpus_id")
-    if not corpus_id:
-        return utils.error_response("No corpus ID provided!"), 404
-
     annotation_dir = current_app.config.get("CORPORA_DIR") + "/" + corpus_id + current_app.config.get("SPARV_WORK_DIR")
     try:
         oc.delete(annotation_dir)
@@ -182,13 +147,15 @@ def clear_annotations():
 
 
 @bp.route("/list-exports", methods=["GET"])
-def list_exports():
+@utils.login()
+def list_exports(oc, corpora, corpus_id):
     """List exports available for download for a given corpus."""
     return utils.error_response("Not yet implemented!"), 501
 
 
 @bp.route("/download-export", methods=["GET"])
-def download_export():
+@utils.login()
+def download_export(oc, corpora, corpus_id):
     """Download and export for a corpus."""
     # TODO: Need specification! Should result come as zip file? Should user specify which export to download?
     # Should there be a default download (or default = download all available exports)?
